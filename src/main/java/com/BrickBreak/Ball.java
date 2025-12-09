0a592ffc7b91c9596f3264884c8824556a8057b4
Derin-WJ <derinsoysalis2009@gmail.com>
2025-12-09 15:58:54 +0000

    agewjia

--- src/main/java/com/BrickBreak/Ball.java
+++ src/main/java/com/BrickBreak/Ball.java
@@ -5,11 +5,21 @@ public class Ball {
 	//your code here!
 	
 	//don't forget you need instance variables:
+	private int x;
+	private int y;
+	private int radius;
 	
 	
 	
 	//constructor(s):
-	
+	public Ball(int x, int y, int diameter){
+		this.x = x;
+		this.y = y;
+		this.diameter = 2 * radius;
+	}
 	//methods:
-
+	public void draw(Graphics g){
+		g.setColor(Color.BLUE);
+		g.fillOval(x, y, radius, radius);
+	}
 }
